5ead12aa915833903269a13b155148b9c883dcb9
jmather625 <jatinm2@illinois.edu>
2019-11-13 20:01:18 -0600

    added some preprocessing so images are at least complete... not sure how to guarentee that they are as well-colored as possible

--- planet_data/PlanetDownloader.py
+++ planet_data/PlanetDownloader.py
@@ -5,6 +5,7 @@ from requests.auth import HTTPBasicAuth
 import os
 import json
 from io import BytesIO
+from shapely.geometry import Polygon
 
 
 class PlanetDownloader:
@@ -12,25 +13,11 @@ class PlanetDownloader:
         self.api_key = api_key
         self.item_type = item_type
     
-    def create_cords(lat, lon):
-        amt = 0.0005
-        lat_down = lat - amt*10
-        lat_up = lat + amt*10
+    def create_cords(lat, lon, zoom):
+        xtile, ytile = deg_to_tile(lat, lon, zoom)
 
-        lon_left = lon - amt/10
-        lon_right = lon + amt/10
-
-        return [[lon_left, lat_down], [lon_right, lat_down], [lon_right, lat_up], [lon_left, lat_up]]
-    
-    
-    def deg_to_tile(lat_deg, lon_deg, zoom):
-        """Converts coordinates into the nearest x,y Slippy Map tile"""
-        lat_rad = math.radians(lat_deg)
-        n = 2.0 ** zoom
-        xtile = int((lon_deg + 180.0) / 360.0 * n)
-        ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad)))
-                     / math.pi) / 2.0 * n)
-        return (xtile, ytile)
+        coords = [tilexy_to_deg(xtile, ytile, zoom, a, b) for a,b in [(0,0), (0,255), (255,255), (255,0)]]
+        return [[b,a] for a,b in coords]
         
     def download_image(self, lat, lon, min_year, min_month, max_year, max_month, zoom=14):
         if min_month < 10:
@@ -39,10 +26,11 @@ class PlanetDownloader:
         if max_month < 10:
             max_month = '0' + str(max_month)
         
+        cords = PlanetDownloader.create_cords(lat, lon, zoom)
         geo_json_geometry = {
           "type": "Polygon",
           "coordinates": [
-              PlanetDownloader.create_cords(lat, lon)
+              cords
           ],
 
         }
@@ -69,7 +57,7 @@ class PlanetDownloader:
           "type": "RangeFilter",
           "field_name": "cloud_cover",
           "config": {
-            "lte": 0.2
+            "lte": 0.05
           }
         }
 
@@ -93,18 +81,29 @@ class PlanetDownloader:
             json=search_endpoint_request)
         
         res = json.loads(result.text)
+        
+        x, y = deg_to_tile(lat, lon, zoom)
         item_id = None
 
         if len(res['features']) == 0:
             print('No image found, try widening your search or using a different satellite')
-            return
+            return None
         else:
-            # choose middle idx
-            idx = len(res['features']) // 2
-            item_id = res['features'][idx]['id']
-            
-            
-        x, y = PlanetDownloader.deg_to_tile(lat, lon, zoom)
+            polya = Polygon(cords) 
+            b_cords = [tilexy_to_deg(x,y,zoom,a,b) for a,b in [(0,0), (1,0), (1,1), (0,1)]]
+            polyb = Polygon([(b,a) for (a,b) in b_cords])
+
+            for idx in range(len(res['features'])):
+                polyc = Polygon(res['features'][idx]['geometry']['coordinates'][0])
+                
+                if polyc.contains(polya) and polyc.contains(polyb):
+                    item_id = res['features'][idx]['id']
+                    break
+        
+        if item_id is None:
+            print('No good images found')
+            return None
+        
         url = 'https://tiles0.planet.com/data/v1/{}/{}/{}/{}/{}.png?api_key={}'.format(self.item_type, item_id, zoom, x, y, self.api_key)
         
         res = requests.get(url)
@@ -113,5 +112,46 @@ class PlanetDownloader:
             return
         
         return Image.open(BytesIO(res.content))
+    
+"""
+Important geoconversion functions
+"""
+
+def tilexy_to_deg(xtile, ytile, zoom, x, y):
+    """Converts a specific location on a tile (x,y) to geocoordinates."""
+    decimal_x = xtile + x / 256
+    decimal_y = ytile + y / 256
+    n = 2.0 ** zoom
+    lon_deg = decimal_x / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * decimal_y / n)))
+    lat_deg = math.degrees(lat_rad)
+    return (lat_deg, lon_deg)
+
+def deg_to_tilexy(lat_deg, lon_deg, zoom):
+    """Converts geocoordinates to an x,y position on a tile."""
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    x = ((lon_deg + 180.0) / 360.0 * n)
+    y = ((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad)))
+        / math.pi) / 2.0 * n)
+    return (int((x % 1) * 256), int((y % 1) * 256))
+
+def tile_to_deg(xtile, ytile, zoom):
+    """Returns the coordinates of the northwest corner of a Slippy Map
+    x,y tile"""
+    n = 2.0 ** zoom
+    lon_deg = xtile / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lat_deg = math.degrees(lat_rad)
+    return (lat_deg, lon_deg)
+
+def deg_to_tile(lat_deg, lon_deg, zoom):
+    """Converts coordinates into the nearest x,y Slippy Map tile"""
+    lat_rad = math.radians(lat_deg)
+    n = 2.0 ** zoom
+    xtile = int((lon_deg + 180.0) / 360.0 * n)
+    ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad)))
+                 / math.pi) / 2.0 * n)
+    return (xtile, ytile)
 
        
