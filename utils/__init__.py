9ae4c452e940cd0279ef77310e18bcb21de20fc4
Jatin Mathur <jatinm2@illinois.edu>
2020-06-07 02:04:30 +0000

    updated famine repo with all the new changes with the model approach and added comparison to lenz et al

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .utils import *
 from .google_downloader import *
 from .planet_downloader import *
 from .ridge_training import *
+from .ridge_ensemble import *
--- utils/ridge_training.py
+++ utils/ridge_training.py
@@ -1,4 +1,8 @@
-# This is based on code from the Jean et al Github that is modified to work with Python3 and our metrics
+'''
+This code contains function that are used to run cross-validation. The models
+created in this file are then ensembled. Some of this code is based
+on code from Jean et al's Github.
+'''
 
 import numpy as np
 import pandas as pd
@@ -20,17 +24,21 @@ def run_randomized_cv(X, y, k=5, k_inner=5, random_seed=7, points=10,
     np.random.seed(random_seed)
     alphas = np.logspace(alpha_low, alpha_high, points)
     r2s = []
+    ridges = []
+    scalers = []
     y_hat = np.zeros_like(y)
     kf = KFold(n_splits=k, shuffle=True)
     fold = 0
     for train_idx, test_idx in kf.split(X):
         if to_print:
             print(f"fold: {fold}", end='\r')
-        r2, y_p = evaluate_fold(X, y, train_idx, test_idx, k_inner, alphas, to_print)
+        r2, y_p, ridge, scaler = evaluate_fold(X, y, train_idx, test_idx, k_inner, alphas, to_print)
         r2s.append(r2)
+        ridges.append(ridge)
+        scalers.append(scaler)
         y_hat[test_idx] = y_p
         fold += 1
-    return np.mean(r2s), y_hat
+    return np.mean(r2s), y_hat, ridges, scalers
 
 
 def scale_features(X_train, X_test):
@@ -40,7 +48,7 @@ def scale_features(X_train, X_test):
     X_scaler = StandardScaler(with_mean=True, with_std=False)
     X_train = X_scaler.fit_transform(X_train)
     X_test = X_scaler.transform(X_test)
-    return X_train, X_test
+    return X_train, X_test, X_scaler
 
 
 def train_and_predict_ridge(alpha, X_train, y_train, X_test):
@@ -50,7 +58,8 @@ def train_and_predict_ridge(alpha, X_train, y_train, X_test):
     ridge = linear_model.Ridge(alpha)
     ridge.fit(X_train, y_train)
     y_hat = ridge.predict(X_test)
-    return y_hat
+    return y_hat, ridge
+
 
 def find_best_alpha(X, y, k_inner, alphas, to_print=False):
     """
@@ -64,8 +73,8 @@ def find_best_alpha(X, y, k_inner, alphas, to_print=False):
         for train_idx, test_idx in kf.split(X):
             X_train, X_test = X[train_idx], X[test_idx]
             y_train, y_test = y[train_idx], y[test_idx]
-            X_train, X_test = scale_features(X_train, X_test)
-            y_hat[test_idx] = train_and_predict_ridge(alpha, X_train, y_train, X_test)
+            X_train, X_test, _ = scale_features(X_train, X_test)
+            y_hat[test_idx], _ = train_and_predict_ridge(alpha, X_train, y_train, X_test)
         r2 = metrics.r2_score(y, y_hat)
         if r2 > best_r2:
             best_alpha = alpha
@@ -82,10 +91,10 @@ def evaluate_fold(X, y, train_idx, test_idx, k_inner, alphas, to_print=False):
     X_train, X_test = X[train_idx], X[test_idx]
     y_train, y_test = y[train_idx], y[test_idx]
     best_alpha = find_best_alpha(X_train, y_train, k_inner, alphas, to_print)
-    X_train, X_test = scale_features(X_train, X_test)
-    y_test_hat = train_and_predict_ridge(best_alpha, X_train, y_train, X_test)
+    X_train, X_test, scaler = scale_features(X_train, X_test)
+    y_test_hat, ridge = train_and_predict_ridge(best_alpha, X_train, y_train, X_test)
     r2 = metrics.r2_score(y_test, y_test_hat)
-    return r2, y_test_hat
+    return r2, y_test_hat, ridge, scaler
 
 
 def run_spatial_cv(X, y, groups, k_inner=5, random_seed=7, points=10,
@@ -98,6 +107,8 @@ def run_spatial_cv(X, y, groups, k_inner=5, random_seed=7, points=10,
     alphas = np.logspace(alpha_low, alpha_high, points)
     k = int(groups.max() + 1)
     r2s = []
+    ridges = []
+    scalers = []
     y_hat = np.zeros_like(y)
     fold = 0
     for i in range(k):
@@ -105,23 +116,25 @@ def run_spatial_cv(X, y, groups, k_inner=5, random_seed=7, points=10,
         test_idx = groups == i
         if to_print:
             print(f"fold: {fold}", end='\r')
-        r2, y_p = evaluate_fold(X, y, train_idx, test_idx, k_inner, alphas)
-        # could use this function to do inner-fold spatial validation
-        # r2, y_p = evaluate_spatial_fold(X, y, groups, train_idx, test_idx, alphas)
+        r2, y_p, ridge, scaler = evaluate_spatial_fold(X, y, groups, train_idx, test_idx, alphas)
+        ridges.append(ridge)
+        scalers.append(scaler)
         r2s.append(r2)
         y_hat[test_idx] = y_p
         fold += 1
-    return np.mean(r2s), y_hat
+    return np.mean(r2s), y_hat, ridges, scalers
+
 
 def evaluate_spatial_fold(X, y, groups, train_idx, test_idx, alphas):
     X_train, X_test = X[train_idx], X[test_idx]
     y_train, y_test = y[train_idx], y[test_idx]
     groups_train = groups[train_idx]
     best_alpha = find_best_alpha_spatial(X_train, y_train, groups_train, alphas)
-    X_train, X_test = scale_features(X_train, X_test)
-    y_test_hat = train_and_predict_ridge(best_alpha, X_train, y_train, X_test)
+    X_train, X_test, scaler = scale_features(X_train, X_test)
+    y_test_hat, ridge = train_and_predict_ridge(best_alpha, X_train, y_train, X_test)
     r2 = metrics.r2_score(y_test, y_test_hat)
-    return r2, y_test_hat
+    return r2, y_test_hat, ridge, scaler
+
 
 def find_best_alpha_spatial(X, y, groups, alphas):
     """
@@ -138,16 +151,20 @@ def find_best_alpha_spatial(X, y, groups, alphas):
             test_idx = groups == g
             X_train, X_test = X[train_idx], X[test_idx]
             y_train, y_test = y[train_idx], y[test_idx]
-            X_train, X_test = scale_features(X_train, X_test)
-            y_hat[test_idx] = train_and_predict_ridge(alpha, X_train, y_train, X_test)
+            X_train, X_test, _ = scale_features(X_train, X_test)
+            y_hat[test_idx], _ = train_and_predict_ridge(alpha, X_train, y_train, X_test)
         r2 = metrics.r2_score(y, y_hat)
         if r2 > best_r2:
             best_alpha = alpha
             best_r2 = r2
     return best_alpha
 
+
 def assign_groups(df, k, random_seed=7):
-    ''' Assign clusters in df (columns cluster_lat, cluster_lon) into k groups, also returns cluster centers'''
+    '''
+    Assign clusters in df (columns cluster_lat, cluster_lon) into k groups
+    also returns cluster centers
+    '''
     np.random.seed(random_seed)
     km = KMeans(k)
     return km.fit_predict(df[['cluster_lat', 'cluster_lon']]), km.cluster_centers_
